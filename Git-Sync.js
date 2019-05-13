c09e36e3cfc563413afcbccdffb26bc1380d28c6
DannoPeters <Danno.Peters@usask.ca>
2019-05-13 15:06:58 -0600

    test

--- Git-Sync.js
+++ Git-Sync.js
@@ -15,8 +15,8 @@ step 9. PROFIT $$$
 var secret = "Very$ecret$ecret"; //Secret for verifying WebHook from Repo-A
 var gitA = "DannoPeters/Repo-A" //Full repo name, used to identify Webhook Sender
 var gitB = "DannoPeters/Repo-B" //Full repo name, used to identify Webhook Sender
-var repoA = "/home/marina/Git_Projects/Repo-A"; //location of repo-A on server
-var repoB = "/home/marina/Git_Projects/Repo-B"; //location of repo-b on server
+var repoA = "/run/media/peters/Danno_SuperDARN/Git_Projects/Repo-A"; //location of repo-A on server
+var repoB = "/run/media/peters/Danno_SuperDARN/Git_Projects/Repo-B"; //location of repo-b on server
 const port = 8080 //specify the port for the server to listen on
 var dir = "hardware_dir" //directory to copy files to in repo-B
 
@@ -28,12 +28,7 @@ let crypto = require(`crypto`); //import crypto library
 //let fetch = require(`node-fetch`) //include fetch so ngrok settings JSOn can be fetched
 const exec = require(`child_process`).exec; //include child_process library so we can exicute shell commands
 
-/*
-//Start ngrok connection, and print out URL. Will start a new server with each exicution
-ngrok.connect(port, function (err, url) {
-    c; 
-});
-*/
+while(1) {
 
 //Webserver Operation
 http.createServer(function (req, res) { //create webserver
@@ -47,19 +42,147 @@ http.createServer(function (req, res) { //create webserver
         var gitID = githubWebHook.repository.id; //ID of the repository
         var gitURL = githubWebHook.repository.html_url; //URL of the repository
 
+        if (req.headers['X-GitHub-Event'] == "push") { //if event type is push run following code
+            switch (gitFullName){
+
+                case gitA: //pull from repo A to local A, and copy from local A to local B
+                    //Print statements to ensure data is read correctly
+                    //console.log(`Commit Message: ` + commitMessage);
+                    //console.log(`Added Files: ` + addedFiles);
+                    //console.log(`Modified Files: ` + modifiedFiles);
+                    //console.log(`Removed Files: ` + removedFiles);
+
+                    //Seperate data from intrest our of JSON dicts and lists
+                    var modifiedFiles = githubWebHook.commits[0].modified; //Create list of files modified in Push
+                    var addedFiles = githubWebHook.commits[0].added; //Create list of files added in Push
+                    var removedFiles = githubWebHook.commits[0].removed; //Create list of files removed in Push
+                    var commitMessage = githubWebHook.commits[0].message; //Read commit message for use in push to repo-B
+
+                    if (req.headers[`x-hub-signature`] == sig) {
+
+                        console.log(`cd ${repoA} && git pull\n`);
+                        var cmd = `cd ${repoA} && git pull`
+                        exec(cmd, (error, stdout, stderr)=> {
+                            if (error) {
+                                console.error(`${cmd}: ${error}\n`);
+                            }
+                        }); //Pull from github repoA to local repo
+
+                        //Copy all modified files to repoB
+                        console.log(`Copy Modified Files`);
+                        for (var file in modifiedFiles) {
+                            console.log(`cp ${repoA}/${modifiedFiles[file]} ${repoB}/${dir}/${modifiedFiles[file]}\n`);
+                            var cmd = `cp ${repoA}/${modifiedFiles[file]} ${repoB}/${dir}/${modifiedFiles[file]}`;
+                            exec(cmd, (error, stdout, stderr)=> {
+                                if (error) {
+                                    console.error(`${cmd}: ${error}\n`);
+                                }
+                            });
+                        }
+
+                        //Copy all new files to repoB
+                        //console.log(`Copy Added Files`);
+                        
+                        var cmd = `cd ${repoB}`
+                        exec(cmd, (error, stdout, stderr)=> {
+                                    if (error) {
+                                        console.error(`${cmd}: ${error}\n`);
+                                    }
+                                });
 
 
+                        //Pull from github repoA to local repo
+                        console.log(`git pull\n`);
+                        var cmd = `git pull`
+                        exec(cmd, (error, stdout, stderr)=> {
+                            if (error) {
+                                console.error(`${cmd}: ${error}\n`);
+                            }
+                        }); 
 
-        
+                        //Copy all added files to new repo
+                        for (var file in addedFiles) {
+                           console.log(`cp ${repoA}/${addedFiles[file]} ${repoB}/${dir}/${addedFiles[file]}\n`);
+                           var cmd = `cp ${repoA}/${addedFiles[file]} ${repoB}/${dir}/${addedFiles[file]}`;
+                           exec(cmd, (error, stdout, stderr)=> {
+                               if (error) {
+                                   console.error(`${cmd}: ${error}\n`);
+                               }
+                           });
+                           var cmd = `git add ${repoB}/${dir}/${addedFiles[file]}`;
+                           console.log(cmd)
+                           exec(cmd, (error, stdout, stderr)=> {
+                                   if (error) {
+                                       console.error(`${cmd}: ${error}`);
+                                   }
+                               });
+                        }
 
-        
-    });
+
+                        ////Commit changes to local repoB with message from GitHub repoA
+                        //console.log(`Commit Changes`);
+                        console.log(`pwd`);
+                        var cmd = `pwd`;
+                        exec(cmd, (error, stdout, stderr)=> {
+                               if (error) {
+                                   console.error(`${cmd}: ${error}\n`);
+                               }
+                               console.log(`${cmd}: ${stdout}`)
+                           }); 
+
+                        console.log(`git branch`);
+                        var cmd = `git branch`;
+                        exec(cmd, (error, stdout, stderr)=> {
+                               if (error) {
+                                   console.error(`${cmd}: ${error}\n`);
+                               }
+                               console.log(`${cmd}: ${stdout}`)
+                           }); 
+
+                        
+                        console.log(`git commit -m ${commitMessage}\n`);
+                        var cmd = `git commit -m ${commitMessage}`;
+                        exec(cmd, (error, stdout, stderr)=> {
+                               if (error) {
+                                   console.error(`${cmd}: ${error}\n`);
+                               }
+                           });
+                        
+                        //Push local repoB to GitHub
+                        console.log(`Push Changes`);
+                        console.log(`git push origin master\n`);
+                        var cmd = `git push origin master`;
+                        exec(cmd, (error, stdout, stderr)=> {
+                               if (error) {
+                                   console.error(`${cmd}: ${error}\n`);
+                               }
+                           });
+                    }
+                    break;
+
+                case gitB: //Verify that push to repo B was correct
+                        testModified = (modifiedFiles == githubWebHook.commits[0].modified);
+                        testAdded = (addedFiles == githubWebHook.commits[0].added);
+                        testRemoved = (removedFiles == githubWebHook.commits[0].removed);
+                        testCommit = (commitMessage == githubWebHook.commits[0].message);
+
+                        if (testModified && testAdded && testRemoved && testCommit) {
+                            console.log(`Git Sync between ${gitA} and ${gitB} was sucessful`);
+
+                        }
+                    break;
+
+                default:
+                break;
+            }
+        }
+
+    }
 
     res.end('');
 }).listen(port, (err) => {
     if (err) return console.log(`Something bad happened: ${err}`);
     console.log(`Node.js server listening on ${port}`);
-
-
-
 });
+});
+}
