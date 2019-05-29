b4db5631961a763445cfeb205ad52cc526ebdab0
Danno Peters <Danno.Peters@usask.ca>
2019-05-29 10:06:51 -0600

    Test file for automated sync testing

--- TestFIles.dat/Testing.js
+++ TestFIles.dat/Testing.js
@@ -1,3 +1,115 @@
-testJSON = JSON.parse('{ "commits": [ {"notmessage": 1, "message": "test" }]}');
-console.log('Commit Message: ' + JSON.stringify(testJSON.commits));
-console.log('Commit Message: ' + testJSON.commits[0].message);
+/User Configuration ***Both Repos MUST have local configuration***
+var secretA = "Very$ecret$ecret"; //Secret for verifying WebHook from RepoA
+var secretB = "AnotherVery$ecret$ecret"; //Secret for verifying WebHook from RepoB
+var gitA = "DannoPeters/Repo-A"; //Full repo name, used to identify Webhook Sender
+var gitB = "DannoPeters/Repo-B"; //Full repo name, used to identify Webhook Sender
+var repoA = "/run/media/peters/Danno_SuperDARN/Git_Projects/Repo-A"; //location of repo-A on server
+var repoB = "/run/media/peters/Danno_SuperDARN/Git_Projects/Repo-B"; //location of repo-b on server
+var gitSync = "/run/media/peters/Danno_SuperDARN/Git_Projects/Git-Sync-NodeJS"; //Location of Git-Sync.js on server
+const port = 8080; //specify the port for the server to listen on
+var dirA = "hdw.dat/" //directory to copy files from in repo-A
+var dirB = "hardware_dir"; //directory to copy files to in repo-B
+var user = "DannoPeters"; //set the github username of the server (configured using ssh)
+
+var actionArray = new Array(); //Array to store information about actions taken
+
+//Import Required
+let http = require(`http`); //import http library
+let crypto = require(`crypto`); //import crypto library
+//let ngrok = require(`ngrok`); //include ngrok to allow through firewall
+//let fetch = require(`node-fetch`) //include fetch so ngrok settings JSOn can be fetched
+var execSync = require(`child_process`).execSync; //include child_process library so we can exicute shell commands
+var fs = require("fs"); //required to write to files
+const dns = require('dns'); //required to resolve domain name for log file
+
+log(`ALL`, `INIT: Testing Program Started`, 0, '\n');
+
+var days = 0;
+var hours = 0;
+var minutes = 0;
+var seconds = 10;
+var ms = 60;
+
+
+
+//runs commands in synchronus (serial) terminal 
+function runCmd(cmd) {
+    log(`OP`, `SYNC: Exicuted ${cmd}`, 2);
+
+    try{ 
+        execSync(`${cmd}`); 
+    }
+    catch(error){
+        log(`ALL`, `ERROR: Terminal Command Failed: ${error}`, 2);
+        return;
+    }
+}
+
+//
+function log (stream, message, level, prefix){
+    prefix = prefix || '';
+    var today = new Date();
+    var operation = fs.createWriteStream(`./hdw.Git-Sync_${today.getUTCFullYear()}_Operation.log`, {flags:'a'});
+    var error = fs.createWriteStream(`./Git-Sync_${today.getUTCFullYear()}_Error.log`, {flags:'a'});
+
+    var date = `${today.getUTCDate()}/${(today.getUTCMonth()+1)}/${today.getUTCFullYear()}`;
+    var time = `${(today.getUTCHours())}:${(today.getUTCMinutes())}:${today.getUTCSeconds()}:${today.getUTCMilliseconds()}`;
+
+    switch (stream){
+        case 'OP':
+            operation.write(`${prefix}${date} ${time} UTC${new Array(level*5+1).join(' ')}    ${message}\n`);
+            operation.end();
+            break;
+
+        case 'ER':
+            error.write(`${prefix}${date} ${time} UTC${new Array(level*5+1).join(' ')}    ${message}\n`);
+            error.end();
+            break;
+
+        case 'ALL':
+            error.write(`${prefix}${date} ${time} UTC${new Array(level*5+1).join(' ')}    ${message}\n`);
+            operation.write(`${prefix}${date} ${time} UTC${new Array(level*5+1).join(' ')}    ${message}\n`);
+            error.end();
+            operation.end();
+            break;
+
+        default:
+            break;
+
+    }
+    
+    }
+
+    function doSomething() {
+    var d = new Date(),
+        h = new Date(d.getFullYear(), d.getMonth(), d.getDate()+ Math.random( )*days, d.getHours() + Math.random( )*hours, d.getMinutes() + Math.random( )*minutes, d.getSeconds() + Math.random( )*seconds, Math.random( )*ms),
+        e = h - d;
+    if (e > 100) { // some arbitrary time period
+        setTimeout(doSomething, e);
+    }
+
+    //Pull from github repoB to local repo
+    var cmd = `cd ${repoA} && git pull`;
+    runCmd(cmd);
+
+    //edit log file posing as hardware file
+    log(`ALL`, `Sending New Test File`, 2, '\n'); 
+    console.log(`Sending New Test File`);
+
+    //add all files to git
+    var cmd = `cd ${repoA} && git add --all`;
+    runCmd(cmd);
+
+    //Commit changes to local repoB with message from GitHub repo
+    var today = new Date();
+    var cmd = `cd ${repoA} && git commit -m "Test of Git-Sync at ${today.getUTCDate()}/${(today.getUTCMonth()+1)}/${today.getUTCFullYear()} ${(today.getUTCHours())}:${(today.getUTCMinutes())}:${today.getUTCSeconds()} UTC"`;
+    runCmd(cmd);
+
+    //Push local repoB to GitHub
+    var cmd = `cd ${repoA} && git push`;
+    runCmd(cmd);
+}
+
+while (1){
+	doSomething();
+}
